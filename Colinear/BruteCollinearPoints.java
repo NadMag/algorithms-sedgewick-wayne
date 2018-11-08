582c5b9a5d49420f3d3e2986da907b201146c41c
NadMag <nadavm243@gmail.com>
2018-11-08 22:24:31 +0200

    Fixed duplicate point checks

--- Colinear/BruteCollinearPoints.java
+++ Colinear/BruteCollinearPoints.java
@@ -5,6 +5,7 @@
  **************************************************************************** */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,35 +16,38 @@ public class BruteCollinearPoints {
 
     public BruteCollinearPoints(Point[] points) {
 
-        nullCheckPoints(points);
-        checkDuplicatePoints(points);
+        Point[] pointsClone = points.clone();
+        Arrays.sort(pointsClone);
 
-        for (int i = 0; i < points.length; i++) {
-            Point orgin = points[i];
+        nullCheckPoints(pointsClone);
+        checkDuplicatePoints(pointsClone);
 
-            for (int j = 0; j < points.length; j++) {
+        for (int i = 0; i < pointsClone.length; i++) {
+            Point orgin = pointsClone[i];
+
+            for (int j = 0; j < pointsClone.length; j++) {
                 if (j == i) continue;
 
-                double firstSlope = orgin.slopeTo(points[j]);
+                double firstSlope = orgin.slopeTo(pointsClone[j]);
 
-                for (int k = 0; k < points.length; k++) {
+                for (int k = 0; k < pointsClone.length; k++) {
                     if (k == i || k == j) continue;
 
-                    if (orgin.slopeTo(points[k]) == firstSlope) {
+                    if (orgin.slopeTo(pointsClone[k]) == firstSlope) {
 
-                        for (int t = 0; t < points.length; t++) {
+                        for (int t = 0; t < pointsClone.length; t++) {
                             if (t == k || t == j || t == i) continue;
 
-                            if (orgin.slopeTo(points[t]) == firstSlope) {
+                            if (orgin.slopeTo(pointsClone[t]) == firstSlope) {
 
                                 LinkedList<Point> linePoints = new LinkedList<>();
-                                linePoints.add(points[i]);
-                                linePoints.add(points[j]);
-                                linePoints.add(points[k]);
-                                linePoints.add(points[t]);
+                                linePoints.add(pointsClone[i]);
+                                linePoints.add(pointsClone[j]);
+                                linePoints.add(pointsClone[k]);
+                                linePoints.add(pointsClone[t]);
 
                                 if (areSorted(linePoints, Point::compareTo)) {
-                                    segments.add(new LineSegment(points[i], points[t]));
+                                    segments.add(new LineSegment(pointsClone[i], pointsClone[t]));
                                 }
                             }
                         }
